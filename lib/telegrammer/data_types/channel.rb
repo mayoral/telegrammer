5b3232ec183c003df540433e0f8ab3881e2cb462
Luis Mayoral <luis@luismayoral.com>
2015-06-27 12:20:22 +0200

    Fixing coercion of channels into users or group chats

--- lib/telegrammer/data_types/channel.rb
+++ lib/telegrammer/data_types/channel.rb
@@ -1,8 +1,8 @@
 module Telegrammer
   module DataTypes
-    class Channel < Telegrammer::DataTypes::Base
-      attribute :id, Integer
-
+    # Custom attribute class to handle chat attribute in Telegrammer::DataTypes::Message
+    class Channel < Virtus::Attribute
+      # Transforms a channel into a User object or GroupChat object
       def coerce(value)
         value.respond_to?(:first_name) ? User.new(value) : GroupChat.new(value)
       end
